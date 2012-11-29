b73817b7164b4ba7e35d8edb4a94254d2d74c8c6
Jonathan Channon <jonathan@jonathanchannon.com>
2012-11-29 09:47:14 +0000

    Organised solution

---
+++ src/Nancy.Demo.Raven/Modules/HomeModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.Demo.RavenDB.Model;
+using Nancy.Demo.RavenDB.Raven;
+using Raven.Client;
+
+namespace Nancy.Demo.RavenDB.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule(IDocumentSession documentSession)
+        {
+            Get["/"] = parameters =>
+                           {
+                              // var documentSession = ravenSessionProvider.GetSession();
+                               var model = documentSession.Load<Person>(1);
+                               
+                               return View["home", model];
+
+                           };
+        }
+    }
+}
